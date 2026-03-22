04d6302c73fe33e49aeab88ab033c3a88f52dcbc
Julian-M <Z_Yuan_@outlook.com>
2026-03-22 15:35:25 +0800

    fix: Fixed a bug where, after running a project in the development environment, the browser could not be minimized to the background and would automatically pop up

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const enableVueDevTools = false
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-  ],
+    enableVueDevTools && vueDevTools(),
+  ].filter(Boolean),
+  server: {
+    open: false,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
